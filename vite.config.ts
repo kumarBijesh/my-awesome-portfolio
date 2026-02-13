175563afe8f72716faa834c40152e6089472a61e
kumarBijesh <brijeshraj6342@gmail.com>
2026-02-13 23:55:17 +0530

    ci: add automatic deployment via GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/my-awesome-portfolio/",
   server: {
     // Use localhost for Windows to avoid IPv6 binding/firewall issues during local dev.
     host: "localhost",
